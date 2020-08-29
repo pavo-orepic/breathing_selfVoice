b7b2dfcb23a0254ad33ab67888c7f27314902d93
pavo-orepic <pavo.orepic@epfl.ch>
2020-08-29 06:57:38 +0200

    Update ecg.m

--- scripts/ecg.m
+++ scripts/ecg.m
@@ -1,26 +1,22 @@
 function [data] = ecg(data, nSub, minPeakHeight, ... 
     minPeakDistance, lowPassFr, highPassFr, samplingRate)
-    % Receives a row of a subject's data and computes continuous phase of
-    % heartbeat [0, 2Pi] and 2 versions of the binary phase, based on 2
-    % different papers - Hyeong (binaryPhase1) and Kunzendorf
-    % (binaryPhase2). Computes heartRate and heartRatevariability as well.
-    % binary phase: 1 = systole, 2 = diastole, 0 = neither
-    % 
-    % inputs are: mean peak values, distance between peaks and bandpass
-    % filter frequencies. They should be:
+	% receives data structure that contains raw heartbeat recordings (from Biopac MP36R system) and trigger locations
+	%
+    % computes heartbeat continuous and binary phase,
+    % as well as heartbeat rate and rate variability
+    % continuous phase: [0, 2*pi]
+    % binary phase:  based on Kunzendorf, et al., "Active information sampling varies across the cardiac cycle"
+	%
+    % inputs are: minimum R-peak values, min distance between R-peaks and bandpass filter frequencies.
+    % They should be:
     %     minPeakHeight = 0.23;
     %     minPeakDistance = 800; %samples
     %     samplingRate = 2000;
     %     lowPassFr = 1;
     %     highPassFr = 40;
     
-    % bandpass filter
-    d =  designfilt('bandpassiir','FilterOrder',8, ...
-    'HalfPowerFrequency1',lowPassFr,'HalfPowerFrequency2',highPassFr, ...
-    'SampleRate',samplingRate);
-
     for s = 1:nSub
-        %ecg_data = dataSubject.heartbeat;
+		% bandpass filter
         data(s).bp_heartbeat = ft_preproc_bandpassfilter(data(s).heartbeat, samplingRate, [lowPassFr highPassFr],2,'but');
 
 
@@ -28,16 +24,15 @@ function [data] = ecg(data, nSub, minPeakHeight, ...
         [qrspeaks,peak_locations] = findpeaks(data(s).bp_heartbeat,'MinPeakHeight',minPeakHeight,...
             'MinPeakDistance',minPeakDistance);
 
-        trigger_index = data(s).T2_index;
-        %click_index = data(s).T3_index;
+        trigger_index = data(s).trigger;
 
         continuousPhase = [];
-        binaryPhase1 = [];
-        binaryPhase2 = [];
-        %click_phase = [];
+        binaryPhase = [];
+		timeAfterRpeak = [];
+
 
         for i = 1:length(trigger_index)
-            % for all he intervals between 2 peaks.
+            % for all the intervals between 2 peaks.
             for j = 1:length(peak_locations)-1
                 % If the trigger is between them, compute the phase
                 if trigger_index(i) >= peak_locations(j) && trigger_index(i) < peak_locations(j+1)
@@ -51,54 +46,7 @@ function [data] = ecg(data, nSub, minPeakHeight, ...
                     timeAfterRpeak(i) = (trigger_index(i) - peakT1) / samplingRate; 
 
 
-                    % ----------------%
-                    %  Hyeong's paper %
-                    % ----------------%
-                    binaryPhase1(i) = 0; % neither systole nor diastole
-
-                    % -------
-                    % systole 
-                    % -------
-                    % Systole: —200 before R peak to +50ms after R peak 
-
-                    sys_beg_time1 = 0; 
-                    sys_end_time1 = 0.050;
-
-                    sys_beg1 = sys_beg_time1 * samplingRate + peakT1;
-                    sys_end1 = sys_end_time1 * samplingRate + peakT1;
-
-                    sys_beg_time2 = 0.2; 
-                    %sys_end_time2 = 1;
-
-                    sys_beg2 = peakT2 - sys_beg_time2 * samplingRate;
-                    sys_end2 = peakT2;
-
-
-                    % --------
-                    % diastole
-                    % ---------
-                    % Diastole:  +150 ms to +450 ms after R peak
-
-
-                    dia_beg_time = 0.150; 
-                    dia_end_time = 0.450;
-
-                    dia_beg = dia_beg_time * samplingRate + peakT1;
-                    dia_end = dia_end_time * samplingRate + peakT1;
-
-                    if trigger_index(i) >= sys_beg1 && trigger_index(i) <= sys_end1
-                        binaryPhase1(i) = 1; % systole 
-                    elseif trigger_index(i) >= sys_beg2 && trigger_index(i) <= sys_end2
-                        binaryPhase1(i) = 1; % systole 
-                    elseif trigger_index(i) >= dia_beg && trigger_index(i) <= dia_end
-                        binaryPhase1(i) = 2; % diastole 
-                    end 
-
-
-                    % --------------------%
-                    %  Kunzendorf's paper %
-                    % --------------------%
-                    binaryPhase2(i) = 0; % neither systole nor diastole
+                    binaryPhase(i) = 0; % neither systole nor diastole
 
                     % -------
                     % systole 
@@ -107,11 +55,11 @@ function [data] = ecg(data, nSub, minPeakHeight, ...
                     % end: 296–375  ms (M = 329, SD = 22.2) after the R peak
 
 
-                    sys_beg_time_2 = 0.065; 
-                    sys_end_time_2 = 0.329;
+                    sys_beg_time = 0.065; 
+                    sys_end_time = 0.329;
 
-                    sys_beg_2 = sys_beg_time_2 * samplingRate + peakT1;
-                    sys_end_2 = sys_end_time_2 * samplingRate + peakT1;
+                    sys_beg = sys_beg_time * samplingRate + peakT1;
+                    sys_end = sys_end_time * samplingRate + peakT1;
 
 
                     % --------
@@ -120,15 +68,15 @@ function [data] = ecg(data, nSub, minPeakHeight, ...
                     % beginning: 346–425 ms (M = 379, SD = 22.2) after the R peak 
                     % end: 652–1101 ms (M = 805, SD = 129) after the R peak
 
-                    dia_beg_time_2 = 0.379; 
-                    dia_end_time_2 = 0.805;
+                    dia_beg_time = 0.379; 
+                    dia_end_time = 0.805;
 
-                    dia_beg_2 = dia_beg_time_2 * samplingRate + peakT1;
-                    dia_end_2 = dia_end_time_2 * samplingRate + peakT1;
+                    dia_beg = dia_beg_time * samplingRate + peakT1;
+                    dia_end = dia_end_time * samplingRate + peakT1;
 
-                    if trigger_index(i) >= sys_beg_2 && trigger_index(i) <= sys_end_2
+                    if trigger_index(i) >= sys_beg && trigger_index(i) <= sys_end
                         binaryPhase2(i) = 1; % systole 
-                    elseif trigger_index(i) >= dia_beg_2 && trigger_index(i) <= dia_end_2
+                    elseif trigger_index(i) >= dia_beg && trigger_index(i) <= dia_end
                         binaryPhase2(i) = 2; % diastole 
                     end
                     
@@ -136,33 +84,36 @@ function [data] = ecg(data, nSub, minPeakHeight, ...
             end
         end
 
-        %data(s).bp_heartbeat = ecg_data_bpf_1_40;
         data(s).ecgContinuousPhase = continuousPhase;
-        data(s).ecgBinaryPhase1 = binaryPhase1;
-        data(s).ecgBinaryPhase2 = binaryPhase2;
+        data(s).ecgBinaryPhase = binaryPhase;
         data(s).timeAfterRpeak = timeAfterRpeak;
 
-    %     % heart rate
-    %     duration = length(data(s).heartbeat)/samplingRate;
-    %     nr_peaks = length(qrspeaks);
-    %     data(s).ecgRate = nr_peaks/duration*60; %beats per minute
-    %     
-    %     % heartrate variability
-    %     % (Implementation by Arthur Barakat, a student who did a project with me)
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\ECG_preprocess\biosig4octmat-3.0.7
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\ECG_preprocess\biosig4octmat-3.0.7\biosig\t300_FeatureExtraction
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\ECG_preprocess\biosig4octmat-3.0.7\biosig\t250_ArtifactPreProcessingQualityControl
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\HRV_Stuff\HRVAS
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\ECG_preprocess\biosig4octmat-3.0.7\biosig\t200_FileAccess
-    %     addpath(genpath('ECG_preprocess/biosig4octmat-3.0.7'));
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\ECG_preprocess\biosig4octmat-3.0.7\NaN\inst
-    %     addpath D:\student_lab_immersion\heartbeat\BiopacAnalysis\ECG_preprocess\biosig4octmat-3.0.7\tsa\inst
-    %     
-    %     % Compute the HRV. RMSSD is used generally as a score for HRV
-    %     % NOTE : the biosig_HRV function is modified to add the biopack toolbox
-    %     % path and remove it after use to prevent shadowing of other functions.
-    %     [HRV_struct, ~,~,~]=biosig_HRV(data(s).heartbeat,samplingRate);
-    %     data(s).ecgRateVariability = HRV_struct.RMSSD;
+        % heart rate
+        duration = length(data(s).heartbeat)/samplingRate;
+        nr_peaks = length(qrspeaks);
+        data(s).ecgRate = nr_peaks/duration*60; %beats per minute
+        
+        % heartrate variability
+		addpath(genpath('ECG_preprocess/biosig4octmat-3.0.7'));
+		detect_struct = qrsdetect(data(s).heartbeat,samplingRate,2);
+		EVENT = detect_struct.EVENT;
+		r_onsets = EVENT.POS(EVENT.TYP==hex2dec('0501'))/EVENT.SampleRate;
+		r_on = EVENT.POS(EVENT.TYP==hex2dec('0501'));
+		r_diffs = diff(r_onsets);
+		temp_on=r_onsets(1);
+		r_onsets(1)=[];
+		ibi = [r_onsets, r_diffs];
+
+		% remove ectopic ibi with HRVAS methods
+		outliers = locateOutliers(ibi(:,1),ibi(:,2),'sd',3);
+		ibi(:,2)=replaceOutliers(ibi(:,1),ibi(:,2),outliers,'spline',nan);
+			
+		new_r_onsets = cumsum([temp_on;ibi(:,2)]);
+		EVENT.POS(EVENT.TYP==hex2dec('0501')) = new_r_onsets*EVENT.SampleRate;
+		detect_struct.EVENT=EVENT;
+		HRV_struct = heartratevariability(detect_struct);
+		data(s).ecgRateVariability = HRV_struct.RMSSD;
+
+		rmpath(genpath('ECG_preprocess/biosig4octmat-3.0.7'));
     end 
 end
